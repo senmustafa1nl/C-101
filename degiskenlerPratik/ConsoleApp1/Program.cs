329fbeb4366f0a107dff39bd19fe5f9e1e3516a0
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-22 16:59:39 +0100

    Değişkenler Pratik
    Kullanıcı Bilgileri Girişi ve Kayıt Oluşturma
    Bu C# programı, kullanıcıdan kişisel bilgilerini (TC Kimlik Numarası, Adı, Soyadı, Yaş ve Telefon Numarası) alarak bir kayıt işlemi simüle etmektedir. Kullanıcı bilgilerini girdikten sonra, programa girilen bilgilerle ilgili bir kayıt mesajı oluşturulmakta ve telefon numarasına bildirim gönderildiği belirtilmektedir.
    
    Programın İşleyişi:
    
    Kullanıcıdan aşağıdaki bilgilerin alınması istenir:
    TC Kimlik Numarası
    Adı
    Soyadı
    Yaşı
    Telefon Numarası
    Kullanıcıdan alınan veriler, konsola yazdırılır.
    Kullanıcı bilgileri ile bir kayıt mesajı oluşturulur.
    Telefon numarasına bildirim gönderildiği bilgisi ekrana yazdırılır.
    Amaç:
    
    Kullanıcıdan veri almak ve bu verileri kullanarak konsola çıktılar oluşturmak.
    Kullanıcı girişini işlemek için temel Console.ReadLine() ve Console.WriteLine() komutlarını kullanmak.
    Kodun Detayları:
    
    Console.ReadLine(): Kullanıcıdan veri almak için kullanılır.
    Console.WriteLine(): Kullanıcıya çıktı vermek için kullanılır.
    String interpolasyonu: Girilen bilgilerin ekrana düzgün bir şekilde yazdırılmasını sağlar. (Örneğin: {tckn} ve {ad} gibi.)

---
+++ degiskenlerPratik/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Lutfen Asagidaki Bilgileri Giriniz : ");
+		Console.WriteLine("TC Kimlik Numarasi :" );
+		string tckn = Console.ReadLine();
+		Console.WriteLine("Adi :");
+		string ad = Console.ReadLine();
+		Console.WriteLine("Soyadi :" ) ;
+		string soyadi= Console.ReadLine();
+		Console.WriteLine("Yas :");
+		string yas = Console.ReadLine();
+			Console.WriteLine("Telefon Numaraniz :");
+		string telno=Console.ReadLine();
+		Console.WriteLine("--------------------------------------------------------------------");
+		Console.WriteLine($"{tckn} TC Numarali {ad} {soyadi} isimli kisi icin kayit olusturulmustur. ");
+		Console.WriteLine($"{telno} Telefon numarasina bildirim mesaji gonderilmistir.");
+
+		
+		
+		
+		
+		
+			
+	}
+}
