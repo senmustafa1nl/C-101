371d5c362f5f2c5d89bf81b3a32547f5dbd73978
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-22 17:10:33 +0100

    ilk Uygulamam .
    Kendimi tanittigim ve konsola ilk defa komut gönderdiğim uygulama. Staj programinda neden bulunduğumu belirttim . iki satirlik bir uygulama fakat c# gelistirmeye ettigim ilk adımlar

---
+++ Ilk Uygulamam/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Mustafa Sen");
+Console.WriteLine("Tecrube eksigimi kapatmak ve yeni teknolojiler ogrenmek icin burdayim.");
