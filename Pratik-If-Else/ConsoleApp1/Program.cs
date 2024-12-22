b308af884b301131a8bf0c352ab5969038a5ab15
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-22 18:51:40 +0100

    Pratik if-else
    Bu proje, C# dilinde if-else yapısını kullanarak basit bir kontrol akışı uygulaması oluşturmak amacıyla yazılmıştır. Kodda, kullanıcıdan alınan verilere göre belirli koşullara bağlı olarak farklı mesajlar veya işlemler gerçekleştirilir. Bu uygulama, if-else koşul yapısının temel kullanımını öğrenmek ve pratik yapmak isteyenler için örnek teşkil etmektedir.
    
    Özellikler:
    
    Kullanıcıdan sayı veya metin gibi girdiler alır.
    Girilen verilere göre koşul değerlendirmesi yapar.
    Koşullara bağlı olarak farklı çıktılar verir.
    Kullanım:
    
    C# dilinde yazılmış olan kodu bilgisayarınızda çalıştırarak if-else yapısının nasıl işlediğini gözlemleyebilirsiniz.
    Farklı koşullar ekleyerek programı geliştirip test edebilirsiniz.
    Teknolojiler:
    
    C# (C Sharp)
    .NET Framework

---
+++ Pratik-If-Else/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+		Console.WriteLine("Lutfen Bir Sayi Giriniz :");
+		 int Sayi = Convert.ToInt32(Console.ReadLine());
+		if  (Sayi >= 10 )
+		{
+		Console.WriteLine("Girilen Sayi 10'dan Buyuktur. ");
+		
+		}else if(Sayi <= 10 && Sayi >= 0){
+			Console.WriteLine("Girilen Sayi 10'dan Kucuktur.");
+	
+		}
+		else{
+			Console.WriteLine("Gecersiz Sayi Girdiniz.");
+		}
+		if ( Sayi % 2 == 0){
+			Console.WriteLine("Girdiginiz Sayi Cifttir.");
+	
+			
+		}else {
+			Console.WriteLine("Girdiginiz Sayi Tektir.");
+		
+		}
+	}
+}
