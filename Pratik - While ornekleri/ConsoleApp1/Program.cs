b4093b9a80beabcc43156ca1df08c0a45df86c2a
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-23 18:38:02 +0100

    Pratik- While Ornekleri
    Bu proje, C# dilinde while döngüsü kullanarak temel programlama becerilerini geliştirmeyi amaçlayan bir dizi alıştırmayı içermektedir. 5 aşamadan oluşan bu uygulamalarda, while döngüsü ile çeşitli sayısal işlemler gerçekleştirilmiştir. Her aşama, döngülerin ve koşulların nasıl çalıştığını anlamak için basit fakat etkili örnekler sunmaktadır.
    
    Alıştırmalar:
    
    "Kendime inanıyorum, ben bu yazılım işini hallederim!" mesajı, 10 kez ekrana yazdırılmıştır.
    1 ile 20 arasındaki tam sayılar, while döngüsü ile sırasıyla konsola yazdırılmıştır.
    1 ile 20 arasındaki sadece çift sayılar yazdırılmıştır.
    50 ile 150 arasındaki sayıların toplamı hesaplanıp ekrana yazdırılmıştır.
    1 ile 120 arasındaki tek ve çift sayıların toplamları ayrı ayrı hesaplanarak ekrana yazdırılmıştır.
    Proje Özellikleri:
    
    While Döngüsü kullanılarak koşul bazlı işlemler yapılmıştır.
    Her bir aşama, kullanıcıya döngülerin nasıl işlediğini ve koşulların nasıl kontrol edileceğini öğretmek için tasarlanmıştır.
    Temel programlama becerilerini geliştirmek için pratik yapılmış ve kodu basit tutarak anlaşılabilir hale getirilmiştir.
    Kullanım:
    
    Visual Studio veya başka bir C# geliştirme ortamında projeyi açın.
    Uygulama çalıştırıldığında, her bir aşamanın çıktısını konsol ekranında görebilirsiniz.
    Kendi testlerinizi yapmak veya projeyi geliştirmek için kod üzerinde değişiklikler yapabilirsiniz.
    Teknolojiler:
    
    C# (C Sharp)
    .NET Framework veya .NET Core

---
+++ Pratik - While ornekleri/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//Pratik While Ornekleri
+		//1 -> Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+		int i = 0;
+		while (i <= 10)
+		{
+			Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+			i ++ ;
+		}
+		//2 -> 1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+		int sayi2 = 1;
+		while (sayi2 <= 20)
+		{
+		Console.WriteLine(sayi2);
+			sayi2 ++ ;
+		}
+		//3 -> 1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+		int sayi3 = 1;
+		while (sayi3 <=20)
+		{
+		if (sayi3 % 2 == 0)
+		{
+			Console.WriteLine(sayi3);
+			sayi3 ++ ;
+		}else {
+		sayi3 ++ ;
+		}
+		}
+		//4 -> 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız
+		int sayi4 = 50;
+		int toplam = 0;
+		while (sayi4 <= 150)
+		{
+			toplam += sayi4;
+			sayi4 ++ ;
+		}
+		Console.WriteLine(toplam);
+		
+		//5 -> 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+		int tekSayi = 0;
+		int ciftSayi = 0;
+		int sayi5 = 1;
+		while (sayi5 <= 120)
+		{
+		if (sayi5 %2 == 0)
+		{
+			ciftSayi += sayi5;
+			sayi5 ++ ;
+		}else 
+		{
+		tekSayi += sayi5;
+			sayi5 ++ ;
+		}
+		}
+	
+		Console.WriteLine($"Tek Sayilarin Toplami {tekSayi} ") ;
+		Console.WriteLine($"Cift Sayilarin Toplami {ciftSayi}");
+
+	}
+}
