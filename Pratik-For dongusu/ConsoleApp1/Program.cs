b867b093c77f8c8748732979082ca4b06438a577
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-23 17:42:55 +0100

    Pratik - For dongusu
    Bu proje, C# dilinde for döngüsü kullanarak çeşitli programlama alıştırmalarını gerçekleştirmek amacıyla hazırlanmıştır. Toplamda 5 farklı aşamadan oluşan bu alıştırmalar, for döngüsü ve kontrol yapılarını kullanarak temel programlama becerilerini geliştirmeye yöneliktir.
    
    Alıştırmalar:
    
    "Kendime inanıyorum, ben bu yazılım işini hallederim!" mesajı, 10 kere konsol ekranına yazdırılmıştır.
    1 ile 20 arasındaki sayılar konsol ekranına yazdırılmıştır.
    1 ile 20 arasındaki çift sayılar konsol ekranına yazdırılmıştır.
    50 ile 150 arasındaki sayıların toplamı hesaplanmış ve ekrana yazdırılmıştır.
    1 ile 120 arasındaki tek ve çift sayıların toplamları ayrı ayrı hesaplanmış ve ekrana yazdırılmıştır.
    Kullanım:
    
    C# geliştirme ortamında (Visual Studio veya başka bir IDE) projeyi açın.
    Uygulamayı çalıştırarak her aşamanın çıktısını konsol ekranında görebilirsiniz.
    Projeyi geliştirerek farklı sayılarla veya döngüsel yapılarla testler yapabilirsiniz.
    Teknolojiler:
    
    C# (C Sharp)
    .NET Framework veya .NET Core
    Not:
    Bu projede for döngüsü kullanılarak temel sayılarla işlemler gerçekleştirilmiştir. Her alıştırma, for döngüsü ve koşul ifadeleri (if-else) kullanarak problem çözme becerilerini geliştirmeyi amaçlamaktadır. Bu tür alıştırmalar, programlamanın temel yapılarını öğrenmek isteyenler için oldukça faydalıdır.

---
+++ Pratik-For dongusu/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//konsola 10 kere Kendime Inaniyorum, Ben Bu yazilim Isini Hallederim Yazdirmqk	
+		for (int i=0; i<10 ; i++)
+		{
+			Console.WriteLine("Kendime Inaniyorum, Ben Bu yazilim Isini Hallederim ");
+		}
+		//konsola 1-20 arasi sayilari yazdirma
+		for (int i = 1; i <= 20; i ++)
+		{
+		Console.WriteLine(i);
+		}
+		//1-20 arasindaki cift sayilari komnsola yazdirma
+		for (int i =0; i <=20 ; i+=2) 
+		{
+		Console.WriteLine(i);
+		}
+		//50-150 arasindaki sayilari konsola yazdirma
+		int toplam = 0;
+		for (int i=50; i <= 150 ; i++ )
+			
+		{
+		Console.WriteLine(toplam+= i);
+		}
+		//1-120 arasindaki cift ve tek sayilarin toplamlarini ayri ayri ekrana yazdiracagiz
+		int tekToplam = 0;
+		int ciftToplam = 0;
+		for (int i = 0; i <= 120 ; i++)
+		{
+		if (i%2==0){
+			ciftToplam += i;
+		}else{
+		tekToplam += i;
+		}
+		}
+		Console.WriteLine("Tek Sayilarin  Toplami" + tekToplam );
+		Console.WriteLine("Cift Sayilarin Toplami"+ ciftToplam );
+		
+		
+	}
+	
+}
