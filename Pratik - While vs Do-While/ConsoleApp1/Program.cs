92bb6752b29fce7435c55bc5b788c94c1a39ea32
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-24 00:43:54 +0100

    Pratik - While vs Do-While
    Bu verdiğiniz C# kodu, while ve do-while döngülerinin nasıl çalıştığını gösteren bir örnek. Kodda, kullanıcıdan bir sayı alınarak, bu sayıya kadar döngülerin her iki türüyle "Ben Patika'liyim" mesajını ekrana yazdırıyorsunuz.
    
    Kodun çalışma mantığını ve her iki döngü türünün farkını kısaca açıklayalım:
    
    Açıklama:
    While Döngüsü:
    Kullanıcıdan bir sayı alındığında, bu sayı limit olarak belirleniyor. i değeri 0'dan başlayarak, limit'e kadar olan her adımda "Ben Patika'liyim" yazdırılır. Bu döngü, koşul sağlandığı sürece çalışır ve koşulun başta kontrol edilmesi özellik gösterir. Yani döngü başlamadan önce koşul kontrol edilir.
    Do-While Döngüsü:
    Yine kullanıcıdan bir sayı alınır ve bu sayı limit1 olarak belirlenir. i1 değeri 0'dan başlar. Burada önemli olan, do-while döngüsünde, koşul kontrolü döngü sonunda yapılır, yani en az bir kere döngü çalışır, koşul sağlansa da sağlanmasa da ilk "Ben Patika'liyim" mesajı ekrana yazdırılır.

---
+++ Pratik - While vs Do-While/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		//while ile olan kodumuz
+		Console.WriteLine("Sana Bir Sey Soylemek Istiyorum. Ogrenmek Icin Lutfen Bir sayi Gir !!");
+		int limit = Convert.ToInt32(Console.ReadLine());
+		int i = 0 ;
+		while ( i <= limit )
+		{
+			Console.WriteLine("Ben Patika'liyim");
+			i++;
+		}
+		//do-while ile olan kodumuz
+		Console.WriteLine("Sana Bir Sey Soylemek Istiyorum. Ogrenmek Icin Lutfen Bir sayi Gir !!");		
+		int limit1 = Convert.ToInt32(Console.ReadLine());
+		int i1 = 0 ;
+		do {
+		Console.WriteLine("Ben Patika'liyim");
+		i1++;
+		
+		}while(i1 <= limit1);
+		
+	}
+}
