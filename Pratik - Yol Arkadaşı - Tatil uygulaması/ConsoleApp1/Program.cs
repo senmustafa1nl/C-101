40ab3cc1a5c6f2f7c76d89ac4a8cc8e6c5bd71cc
Mustafa SEN <senmustafa1nl@gmail.com>
2024-12-24 12:36:33 +0100

    Pratik - Yol Arkadaşı : Tatil uygulaması
    Bu C# programı, kullanıcıdan çeşitli bilgiler alarak bir turizm rezervasyonu yapmaya yönelik bir konsol uygulaması simüle etmektedir. Uygulama, kullanıcının seçtiği tatil destinasyonuna göre fiyat hesaplamaları yapar, ziyaretçi sayısını ve ulaşım tercihini alır, son olarak toplam fiyatı hesaplayarak kullanıcıya gösterir.
    
    Adım Adım Açıklama:
    
    Hoşgeldiniz Mesajı ve Kullanıcı Bilgisi Alma: Program, kullanıcıya başta hoşgeldiniz mesajı verir. Ardından, ad ve soyad bilgisi kullanıcıdan alınır:
    Paket Seçenekleri ve Fiyat Bilgisi: Kullanıcıya sunulan üç tatil destinasyonu (Bodrum, Marmaris, Çeşme) ve başlangıç fiyatları hakkında bilgi verilir. Bu paketlerin fiyatları bir switch yapısıyla kullanıcı tarafından seçilen destinasyona göre belirlenir:
    
    switch: Kullanıcıdan alınan destinasyon ismi küçük harflerle (ToLower()) işlenir, ardından switch-case yapısı kullanılarak fiyat belirlenir.
    Eğer kullanıcı geçersiz bir destinasyon girerse, default bloğuna girilir ve hata mesajı verilir.
    Ziyaretçi Sayısı: Eğer doğru bir destinasyon seçildiyse ve fiyat güncellendiyse, kullanıcıdan ziyaretçi sayısı alınır. Bu sayıya göre toplam fiyat hesaplanır:
    
    
    Burada, kullanıcıdan gelen ziyaretçi sayısı ile fiyat çarpılarak toplam fiyat hesaplanır ve ekranda kullanıcıya gösterilir.
    Ulaşım Seçeneği: Kullanıcıya ulaşım seçeneği sorulur. İki seçenek sunulmuştur:
    Kara yolu (Kişi başı 1500 TL)
    Hava yolu (Kişi başı 4000 TL)
    Kullanıcı, seçtiği ulaşım şekline göre toplam fiyat güncellenir:
    
    Sonuç ve Ödeme Bilgisi: Program, kullanıcının seçtiği bilgilere göre toplam fiyatı hesaplar ve son olarak bir teşekkür mesajı ile ödeme için müşteri temsilcisine yönlendirme yapılır:
    
    
    
    
    Kodun Genel Akışı:
    Kullanıcıya hoş geldiniz mesajı gösterilir ve ad-soyad alınır.
    Kullanıcıdan bir destinasyon seçmesi istenir ve seçilen destinasyonun fiyatı switch-case ile belirlenir.
    Eğer geçerli bir destinasyon girildiyse, kullanıcıdan ziyaretçi sayısı alınır ve toplam fiyat hesaplanır.
    Kullanıcıya ulaşım seçenekleri sunulur ve ulaşım türüne göre fiyat güncellenir.
    Son olarak, toplam fiyat hesaplanır ve kullanıcıya ödeme bilgisi iletilir.
    Özet:
    Bu program, bir turizm şirketinin basit bir rezervasyon sistemini simüle etmektedir. Kullanıcıdan çeşitli bilgiler alarak (ad, soyad, destinasyon, ziyaretçi sayısı, ulaşım tercihi) toplam bir fiyat hesaplanır ve kullanıcıya ödeme talimatı verilir. Bu tür uygulamalar, kullanıcının belirlediği kriterlere göre fiyat hesaplama işlevini yerine getiren etkileşimli uygulamalardır.

---
+++ Pratik - Yol Arkadaşı - Tatil uygulaması/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+	
+
+
+
+        Console.WriteLine ("Patika Turizm'e Hos Geldiniz ! Lutfen Gitmek Istediginiz Destinasyonu seciniz !!");
+       	Console.WriteLine("Lutfen Adinizi ve Soyadinizi Giriniz !") ;
+		string adSoyad =Console.ReadLine() ;
+		Console.WriteLine("Paketlerimiz Su Sekildedir :\n 1- Bodrum (Paket baslangic fiyati 4000 TL) \n 2- Marmaris (Paket baslangic fiyati 3000TL) \n 3- Cesme (Paket baslangic fiyati 5000 TL) ");
+		//istenilen lokasyonu kullanicidan aliyoruz .
+		Console.WriteLine("Lutfen gitmek istediginiz destinasyonu YAZARAK Belirtiniz ");
+		string istenilenLokasyon = Console.ReadLine().ToLower() ;
+		int fiyat = 0 ;
+		//lokasyon girdisine gore fiyat degiskenini switch-case metodu kullanarak guncelliyoruz
+		switch(istenilenLokasyon)
+		{
+			case "bodrum" :
+				fiyat += 4000 ;
+				break ;
+				
+			case "marmaris" :
+				fiyat += 3000 ;
+				break;
+			case "cesme" :
+				fiyat += 5000;
+					break;
+				default : Console.WriteLine("Hatali bir giris yaptiniz !!!") ;
+				break ; 
+		}
+		//eger lokasyon girdisi alindiysa fiyat guncellenecektir ve 0'dan buyuk olacaktir . bu nedenle ziyaretci sayisini sorabiliyoruz
+		
+		if (fiyat > 0 ){
+		Console.WriteLine("Ziyaretci Sayisini Girin Lutfen !") ;
+			//ziyaretci sayisini kullanicidan aliyoruz 
+		int ziyaretciSayisi =Convert.ToInt32(Console.ReadLine());
+		int ziyaretciliFiyat = fiyat * ziyaretciSayisi ;
+			Console.WriteLine($"{istenilenLokasyon} tatili icin {ziyaretciSayisi} kisilik rezervasyon {adSoyad} adina yapilmistir.\n Konaklamanizda 7 gunluk hersey dahil otel hizmetinden faydalanabileceksiniz.\n ");
+			Console.WriteLine("Ulasiminizi Nasil Saglamak Istersiniz ?\n 1-> Kara yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 1500 TL )\n 2 -> Hava yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 4000 TL)\n Lutfen Rakam Olarak Giriniz !");
+			//ulasim seklini kullanicidan alip fiyati guncelliyoruz
+			int ulasim = Convert.ToInt32(Console.ReadLine());
+			if (ulasim == 1){
+			fiyat = ziyaretciSayisi * (fiyat + 1500);
+			
+			}else if (ulasim == 2){
+			
+			fiyat = ziyaretciSayisi * (fiyat + 4000) ;
+			
+			
+			} else {
+				
+				Console.WriteLine("Yanlis bir rakam girdiniz !") ;
+			}
+			
+		}
+		// enson kullaniciya girdigi bilgiler dogrultusunda fiyat verip musteri temsilcisine aktariyoruz
+		Console.WriteLine($"Patika Turizm'i sectiginiz icin Iyi Tatiller Diler ve Tesekkur Ederiz!\n Toplam Fiyat {fiyat} TL\n Odeme icin Lutfen Musteri temsilcimiz ile iletisime geciniz!  ");
+		
+		
+		
+		
+		
+		
+    }
+}
+
+	
