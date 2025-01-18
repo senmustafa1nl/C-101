b14d30c38f06ba61e053d00c71fc6744bcca9c91
Mustafa SEN <senmustafa1nl@gmail.com>
2025-01-18 20:32:10 +0100

    Patika Kafe - Kahve Seçim Listesi
    Bu proje, Patika Kafe için kullanıcıdan 5 farklı kahve türü girmesini isteyen basit bir C# uygulamasıdır. Kullanıcıdan alınan her kahve ismi bir listeye eklenir ve ardından kullanıcıya bu kahve isimleri sırasıyla gösterilir.
    
    Özellikler:
    
    Uygulama, kullanıcıdan 5 farklı kahve türü girmesini ister.
    Girilen kahve isimleri bir List<string> veri yapısında saklanır.
    Kullanıcıdan alınan her kahve adı ekrana yazdırılır.
    Son olarak, tüm girilen kahve isimleri listelenir.
    Akış:
    
    Uygulama başladığında kullanıcıya "Patika Kafe'ye Hoş geldiniz" mesajı gösterilir.
    Kullanıcıdan 5 farklı kahve ismi alınır.
    Her bir kahve ismi ekrana yazdırılır.
    Tüm girilen kahve isimleri bir liste olarak ekrana yazdırılır.
    Kullanıcı Arayüzü:
    
    Konsol Uygulaması olarak çalışır.
    Kullanıcı, her kahve türünü sırayla girer.
    Girilen kahveler ekranda sırasıyla gösterilir.
    Teknolojiler:
    
    C# (Console Application)

---
+++ Pratik - List2 - Kahve Menusu/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Kahve listesi oluşturuluyor
+        List<string> kahveler = new List<string>();
+
+        // Kullanıcıya mesaj gösteriliyor
+        Console.WriteLine("Patika Kafe'ye Hos geldiniz.");
+        Console.WriteLine("Lutfen 5 farkli Kahve cesidi giriniz!");
+
+        // 5 farklı kahve ismi giriliyor
+        string kahve1 = Console.ReadLine();
+        kahveler.Add(kahve1);
+        Console.WriteLine("Kahve 1: " + kahve1);
+
+        string kahve2 = Console.ReadLine();
+        kahveler.Add(kahve2);
+        Console.WriteLine("Kahve 2: " + kahve2);
+
+        string kahve3 = Console.ReadLine();
+        kahveler.Add(kahve3);
+        Console.WriteLine("Kahve 3: " + kahve3);
+
+        string kahve4 = Console.ReadLine();
+        kahveler.Add(kahve4);
+        Console.WriteLine("Kahve 4: " + kahve4);
+
+        string kahve5 = Console.ReadLine();
+        kahveler.Add(kahve5);
+        Console.WriteLine("Kahve 5: " + kahve5);
+
+        Console.WriteLine("");
+
+        // Girilen kahve isimleri ekrana yazdırılıyor
+        Console.WriteLine("Girilen Kahve Isimleri:");
+
+        foreach (string kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
